d69210f9f4d448ed11b553a603c6549191ff9a41
Balding-Brook30 <raykhwon@gmail.com>
2026-04-27 01:45:46 -0400

    first commit
    simple portfolio page to display for clients

---
+++ My Portfolio/app.js
@@ -0,0 +1,16 @@
+const reveals = document.querySelectorAll(".reveal");
+
+function showOnScroll() {
+  const windowHeight = window.innerHeight;
+
+  reveals.forEach((el) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", showOnScroll);
+showOnScroll();
